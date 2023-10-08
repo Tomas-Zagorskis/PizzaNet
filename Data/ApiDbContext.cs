7451230feff1c8c2dc25ee30894ae5eab9353274
Tomas Zagorskis <tomzagos@gmail.com>
2023-10-08 16:56:42 +0300

    feat: add db in memory

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pizzareact.Models;
+
+namespace pizzareact.Data {
+    public class ApiDbContext : DbContext {
+        protected override void OnConfiguring
+      (DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "PizzaDb");
+        }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+
+    }
+}
