4bf91d2bf78eb81a9cbe66fa45ab58b66378d5d6
Tomas Zagorskis <tomzagos@gmail.com>
2023-10-08 16:15:58 +0300

    Add project files.

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pizzareact.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase {
+
+        public object Index() {
+            return new { Test = "hello" };
+        }
+    }
+}
