b213a47da14c27f99193af089a02c8ea74c6c347
Tomas Zagorskis <tomzagos@gmail.com>
2023-10-08 16:24:37 +0300

    feat: add pizzas model

---
+++ Models/Pizza.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pizzareact.Models {
+    public class Pizza {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
