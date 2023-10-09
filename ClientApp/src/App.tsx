d38ea5d7ca938e470c75eddd9671da4e4a089b92
Tomas Zagorskis <tomzagos@gmail.com>
2023-10-09 09:36:13 +0300

    config app for tailwind and shadcn

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { Pizza, PizzaSize, Topping } from './types/types';
+import PizzaOutlineSvg from './components/ui/PizzaOutlineSvg';
 
 function App() {
 	const [pizzas, setPizzas] = useState<Pizza[] | null>(null);
@@ -22,26 +23,7 @@ function App() {
 		return (
 			<div key={pizza.id}>
 				<div className='pizza-img'>
-					<svg
-						className='pizzas-outline'
-						width='300'
-						height='300'
-						viewBox='0 0 260 260'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'>
-						<circle
-							cx='130'
-							cy='130'
-							r='119.5'
-							stroke='#7F8CA3'
-							strokeDasharray='1 3 1 3'></circle>
-						<circle
-							cx='130'
-							cy='130'
-							r='129.5'
-							stroke='#7F8CA3'
-							strokeDasharray='3 6 3 6'></circle>
-					</svg>
+					<PizzaOutlineSvg />
 					<img src={pizza.image} alt={pizza.name} width={260} height={260} />
 				</div>
 				<h2>{pizza.name}</h2>
@@ -60,9 +42,9 @@ function App() {
 		);
 	});
 	return (
-		<>
+		<div>
 			<div className='card'>{pizzasMap}</div>
-		</>
+		</div>
 	);
 }
 
---
+++ ClientApp/src/components/ui/PizzaOutlineSvg.tsx
@@ -0,0 +1,24 @@
+const PizzaOutlineSvg = () => {
+	return (
+		<svg
+			width='300'
+			height='300'
+			viewBox='0 0 260 260'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<circle
+				cx='130'
+				cy='130'
+				r='119.5'
+				stroke='#7F8CA3'
+				strokeDasharray='1 3 1 3'></circle>
+			<circle
+				cx='130'
+				cy='130'
+				r='129.5'
+				stroke='#7F8CA3'
+				strokeDasharray='3 6 3 6'></circle>
+		</svg>
+	);
+};
+export default PizzaOutlineSvg;
